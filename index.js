fc39c560ccad8fe02c066eaa1b0eeeb54d1f842c
Nelaggy <nelaggy@gmail.com>
2022-05-14 15:32:19 +0800

    upgrade pt 2,3,4
    pt2
    - added html and css for listItem
    - added html and css for popupWindow
    - general css changes and fixes
    
    pt3
    - added loadPopup
    - added loadList
    
    pt4
    - added newPoint
    - added editPoint
    - added delPoint

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 let points = [];
 
-function addRow(e, n, i){ //add new point into table where specified
-	
-}
-
-function delRow(i){ //delete existing point from table where specified
-	
-}
-
-function newPoint(i){ //dialog box to add easting and northing of new point
-
+function newPoint(i){ /*dialog box to add easting and northing of new point*/
+	let e,n = loadPopup(null,null);
 	let point = {
-		'E': e,
-		'N': n
+		'e': e,
+		'n': n
 	};
 	points.splice(i,0,point);
-	addRow(e, n, i);
+	loadList();
 }
 
-function editPoint(e, n, i){ //dialog box to modify easting and northing of existing point
-
+function editPoint(e, n, i){ /*dialog box to modify easting and northing of existing point*/
+	points[i] = {'e': e, 'n': n};
+	loadList();
 }
 
-function delPoint(i){ //removes point
+function delPoint(i){ /*removes point*/
 	points.splice(i,1);
-	delRow(i);
+	loadList();
 }
 
-function pointDialog(e,n){
-	
+function loadList(){
+	let $container = $('.point-container');
+	let $pointItem = $("<div class='point-item add-point'></div>"),
+		$pointIcon = $("<div class='point-icon'><span class='material-icons'>place</span></div>"),
+		$pointActions = $("<div class='point-actions'><span class='material-icons action-button'>add</span><span class='material-icons action-button'>edit</span><span class='material-icons action-button'>delete</span></div>");	
+		let reps = points.length
+	for(var i=0; i<reps; i++){
+		let $newPoint = $pointItem;
+		$newPoint.append($pointIcon);
+		let $pointMGR = `<div class='point-mgr'><div class='point-e'>${points[i]['e']}</div><div class='point-n'>${points[i]['n']}</div></div>`;
+		$newPoint.append($pointMGR);
+		$newPoint.append($pointActions);
+		$container.append($newPoint);
+	}
+}
+
+function loadPopup(e,n){
+	if(e != null) {
+		$('#mgr-input-e').val(e);
+	}
+	if(n != null) {
+		$('#mgr-input-n').val(n);
+	}
+	$('.popup-bg').show();
 }
